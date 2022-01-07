d115195bccb59fc184528a356598ec8a4e26f7fa
BaselAhmed <basel20ahmed@gmail.com>
2022-01-08 01:36:47 +0200

    first and last

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Evaluator} from "./evaluator";
+import {Point} from "./point";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Root-Finder';
+
+
+  constructor() {
+
+  }
+
 }
